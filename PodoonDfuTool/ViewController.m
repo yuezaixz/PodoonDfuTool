54b24d2a420b98380e9bef17037221c4e1f64a8e
吴迪玮 <wdw@meitu.com>
2020-11-01 18:12:08 +0800

    更新固件

--- PodoonDfuTool/ViewController.m
+++ PodoonDfuTool/ViewController.m
@@ -23,7 +23,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [BluetoothService sharedInstance].delegate = self;
-    [BluetoothService sharedInstance].otaUrl = @"ZT_H905A_20201016-V2.2.90";
+    [BluetoothService sharedInstance].otaUrl = @"ZT_H905A_20201101-V2.2.83";
 }
 
 -(void)viewDidAppear:(BOOL)animated {
@@ -33,13 +33,9 @@
 
 - (IBAction)actionSelectFirmware:(id)sender {
     NSArray *firmwares = @[
-        @"ZT_H905A_20201016-V2.2.90",
-        @"ZT_H905A_20201007-V2.2.89",
-    @"ZT_H905A_20200719-V2.2.86",
-        @"ZT_H905A_20200524-V2.2.83",
-    @"ZT_H905A_20200427-V2.0.83",
-    @"ZT_H905A_20200425-V2.2.81",
-                            @"ZT_H905A_20200319-V2.0.79",
+        @"ZT_H905A_20201101-V2.2.83",
+        @"ZT_H905A_20201101-V2.2.91",
+        @"ZT_H905A_20201007-V2.2.89"
                            ];
     UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"选择固件"
                                                                              message:@""
